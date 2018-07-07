b46704ec701e80fe67d3aa8a6c77b988e556172e
jsrn <james.srn@gmail.com>
2018-07-07 10:11:08 +0100

    We can use a div for this layout

--- latestnews.php
+++ latestnews.php
@@ -1,40 +1,26 @@
-<TABLE BORDER CELLPADDING="0" background="bocimages/scrollbgdark.gif" width="100%">
-	<TR>
-	<!-- Header or whatever -->
-	<center><a href="https://discord.gg/DUKQRXf" target="_blank"><IMG SRC="bocimages/largebanner.png" border="0"></a><BR></center>
-		<TD valign="top" class="notes">
+<center><a href="https://discord.gg/DUKQRXf" target="_blank"><IMG SRC="bocimages/largebanner.png"></a></center>
 
+<div class="middle-content-container">
+	<?php
+	include_once "bbcode.php";
+	include_once "secrets.php";
 
-<?php
+	$dsn = "mysql:host=$host;dbname=$db;";
+	$pdo = new PDO($dsn, $user, $pass);
+	$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-include_once "bbcode.php";
+	$stmt = $pdo->query("SELECT * FROM `smf_messages` WHERE `ID_BOARD` = 8 GROUP BY ID_TOPIC ORDER BY postertime DESC LIMIT 20");
 
-include_once "secrets.php";
+	while ($covenewsentry = $stmt->fetch())
+	{
+		$time = ToUnix($covenewsentry["posterTime"]);
+		$title = $covenewsentry["subject"];
+		$text = $covenewsentry["body"];
 
-$dsn = "mysql:host=$host;dbname=$db;";
-$pdo = new PDO($dsn, $user, $pass);
-$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+		echo "<FONT COLOR=\"#ECEB1D\"><B><U>" . doUBBC($title) . "</U></B></FONT><BR>";
+		echo doUBBC($text);
 
-$stmt = $pdo->query("SELECT * FROM `smf_messages` WHERE `ID_BOARD` = 8 GROUP BY ID_TOPIC ORDER BY postertime DESC LIMIT 20");
-
-while ($covenewsentry = $stmt->fetch())
-{
-    $time = ToUnix($covenewsentry["posterTime"]);
-	$title = $covenewsentry["subject"];
-	$text = $covenewsentry["body"];
-
-	echo "<FONT COLOR=\"#ECEB1D\"><B><U>" . doUBBC($title) . "</U></B></FONT><BR>";
-	echo doUBBC($text);
-
-	echo "<P>";
-}
-
-echo "</center>";
-
-?>
-
-		</TD>
-	</TR>
-</TABLE>
-
-
+		echo "<P>";
+	}
+	?>
+</div>
